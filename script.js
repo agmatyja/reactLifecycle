4be1ddc4fc9650549eda2bd2b14ee139a6e27cea
Agnieszka Matyja <agmatyja@gmail.com>
2019-05-18 11:49:48 +0200

    modified lifecycle methods

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-var Counter = React.createClass({ 
-    getInitialState: function() {
+var Counter = React.createClass({
 	
-        console.log("getDefaultProps() - przyjęcie komponentów standardowych");
+	getDefaultProps: function() {
+		console.log("getDefaultProps() - przyjęcie komponentów standardowych");
+	},
+	
+    getInitialState: function() {
 		console.log("getInitialState() - ustawienie wartości początkowej komponentów");
-		console.log("componentWillMount() - odświeżenie danych przed ich wyświetleniem, inicjowanie timerów, nasłuchiwanie zdarzeń na elementach DOM");
-		console.log("render() - montowanie komponentu na drzewie DOM, np.: React elements");
-		console.log("componentDidMount() - odczytanie wskazania timerów, gotowy do dalszych działań");
-		console.log("componentWillReceiveProps(nextProps) - przyjęcie aktualizacji komponentów");
-		console.log("shouldComponentUpdate(nextProps, nextState) - przygotowanie do zmian zawartości komponentów");
-		console.log("componentWillUpdate(nextProps, nextState) - zmiana zawartości komponentów, inicjowanie timerów dotyczących update-u");
-		console.log("render() - montowanie zmian na drzewie DOM");
-		console.log("componentDidUpdate(prevProps, prevState) - zmodyfikowane komponenty gotowe do dalszej pracy, odczytanie wskazania timerów dotyczących update-u");
-		console.log("componentWillUnmount() - odpinanie timerów i nasłuchoiwania zdarzeń w elementach DOM");	
-    
 		return {
 		  counter: 0 
 		};
 	},
+		
+	componentWillMount: function() {	
+		console.log("componentWillMount() - odświeżenie danych przed ich wyświetleniem, inicjowanie timerów, nasłuchiwanie zdarzeń na elementach DOM");
+	},
+	
+	componentDidMount: function() {
+		console.log("componentDidMount() - odczytanie wskazania timerów, gotowy do dalszych działań");
+	},
+	
+	componentWillReceiveProps: function() {
+		console.log("componentWillReceiveProps(nextProps) - przyjęcie aktualizacji komponentów");
+	},
+	
+	shouldComponentUpdate: function() {
+		console.log("shouldComponentUpdate(nextProps, nextState) - przygotowanie do zmian zawartości komponentów");
+		return true
+		
+	},
+	
+	componentWillUpdate: function() {
+		console.log("componentWillUpdate(nextProps, nextState) - zmiana zawartości komponentów, inicjowanie timerów dotyczących update-u");
+		console.log("render() - montowanie zmian na drzewie DOM");
+	},
+	
+	componentDidUpdate: function() {
+		console.log("componentDidUpdate(prevProps, prevState) - zmodyfikowane komponenty gotowe do dalszej pracy, odczytanie wskazania timerów dotyczących update-u");
+	},
+	
+	componentWillUnmount: function() {
+		console.log("componentWillUnmount() - odpinanie timerów i nasłuchoiwania zdarzeń w elementach DOM");
+	},		
+	
 	
     increment: function() {
         this.setState({
@@ -31,6 +56,7 @@ var Counter = React.createClass({
     },
 	
 	render: function() {
+		console.log("render() - montowanie komponentu na drzewie DOM, np.: React elements");
         return React.createElement('div', {},
             React.createElement('span', {}, 'Licznik ' + this.state.counter),
 			React.createElement('button', {onClick: this.increment}, '+'),
